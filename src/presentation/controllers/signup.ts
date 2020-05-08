d5915ac15fbb15095814f95213be14995e29011b
douglasmacb <douglasmacbrito@gmail.com>
2020-05-08 08:11:52 -0300

    refactor: improving the way to get fields from request body

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -11,8 +11,6 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const { email } = httpRequest.body
-
       const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
 
       for (const field of requiredFields) {
@@ -20,7 +18,10 @@ export class SignUpController implements Controller {
           return badRequest(new MissingParamError(field))
         }
       }
-      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+
+      const { email, password, passwordConfirmation } = httpRequest.body
+
+      if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'))
       }
       const isValidEmail = this.emailValidator.isValid(email)
