58d2fb202f64d6e21921230f77f8650c9ecbb297
douglasmacb <douglasmacbrito@gmail.com>
2020-05-13 13:55:39 -0300

    feat: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,12 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository'
+import { AccountModel } from '../../../../domain/models/account'
+import { AddAccountModel } from '../../../../domain/usecases/add-account'
+import { MongoHelper } from '../helpers/mongo-helper'
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const collection = MongoHelper.getCollection('accounts')
+    const result = await collection.insertOne(accountData)
+    return MongoHelper.map(result.ops[0])
+  }
+}
