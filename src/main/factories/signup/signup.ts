db350293b997a6b4a576318fc645c9fab2dbbab8
douglasmacb <douglasmacbrito@gmail.com>
2020-08-25 18:08:38 -0300

    refactor: move bcrypt adapter files to bcrypt folder

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,7 +1,7 @@
 import { SignUpController } from '../../../presentation/controllers/signup/signup'
 import { Controller } from '../../../presentation/protocols'
 import { DbAddAccount } from '../../../data/usecases/db-account/db-add-account'
-import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter'
+import { BcryptAdapter } from '../../../infra/criptography/bcrypt-adapter/bcrypt-adapter'
 import { AccountMongoRepository } from '../../../infra/db/mongodb/account-repository/account'
 import { LogControllerDecorator } from '../../decorators/log'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
